05b9d485410558f18e4885781eedc94b13afb763
huynhan03 <huynhnhandanang@gmail.com>
2023-07-02 20:24:35 +0700

    Add API google sign in

--- google-sign-in/src/main/java/com/example/google/sign/in/GoogleSignInController.java
+++ google-sign-in/src/main/java/com/example/google/sign/in/GoogleSignInController.java
@@ -1,22 +1,20 @@
 package com.example.google.sign.in;
 
 
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableDiscoveryClient
+@RestController
 public class GoogleSignInController {
     @GetMapping("/login")
     public String login(@AuthenticationPrincipal OAuth2User principal) {
-        return "redirect:/";
+        return "/";
     }
 
-    @GetMapping("/")
-    public String base() {
-        return "login successfully";
+    @GetMapping
+    public String home(){
+        return "Welcome to google!";
     }
 }
