925d012802f72cf51bcda48dc637bd4559c9c4cd
huynhan03 <huynhnhandanang@gmail.com>
2023-07-02 20:33:41 +0700

    Done

--- feign-client/src/main/java/com/example/feign/client/FeignClientApplication.java
+++ feign-client/src/main/java/com/example/feign/client/FeignClientApplication.java
@@ -3,9 +3,8 @@ package com.example.feign.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @SpringBootApplication
 @Controller
 @EnableFeignClients
+@EnableDiscoveryClient
 public class FeignClientApplication {
 
 	@Autowired
